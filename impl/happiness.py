b127bf84938917bdb39ec95080b1177cdf0cb7a1
Biswas Biplob <Biplob.Biswas@burda-forward.de>
2020-12-14 00:23:34 +0100

    add more options and add helpful roadmap

--- impl/happiness.py
+++ impl/happiness.py
@@ -1,4 +1,4 @@
-from skill_sdk import skill, Response, ask, tell
+from skill_sdk import skill, Response, ask, tell, context, Card
 from skill_sdk.l10n import _
 import requests
 from random import randrange
@@ -6,31 +6,61 @@ from random import randrange
 
 @skill.intent_handler("TEAM_20_HAPPINESS")
 def mood_handler():
-    """
-    :return:
-    """
     try:
         number = randrange(10)
-
+        # TODO: add functionality to create personalized preferences, social/close friends group, to trigger a call.
         if number < 3:
-            # We request a random joke from icndb with time-out set to 10 seconds
-            response = requests.get('http://api.icndb.com/jokes/random', timeout=10)
-            # We parse the response json or raise exception if unsuccessful
-            response.raise_for_status()
-            data = response.json()
-            # We get the joke from the response data
-            joke = data['value']['joke'] if data.get('type') == 'success' else None
-            # We format our response to user or ask for an excuse
-            if joke:
-                msg = _('HAPPINESS_JOKE', joke=joke)
-            else:
-                msg = _('HAPPINESS_RESPONSE_ERROR')
+            # TODO: Maybe add jokes in a database and source from there
+            # Jokes sourced from "https://github.com/derphilipp/Flachwitze"
+            response = requests.get('https://raw.githubusercontent.com/derphilipp/Flachwitze/master/README.md', timeout=10)
+            raw_jokes = response.text.split("##")[1].split("\n")[2:]
+            jokes = [j[2:] for j in raw_jokes]
+
+            msg = _('HAPPINESS_JOKE', joke=jokes[randrange(len(jokes))])
+            response = tell(msg)
         elif number < 6:
-            msg = _("HAPPINESS_YOUTUBE_VIDEO", video_link="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+            # TODO: Add more URL's and if possible learn from user history
+            video_ids = ["DODLEX4zzLQ","dQw4w9WgXcQ","tvMO9TNfdHs"]
+            msg = _("HAPPINESS_CHECK_PHONE")
+            response = tell(msg)
+            response.card = Card(
+                title=_("HAPPINESS_TITLE"),
+                subtitle=_("HAPPINESS_SUB_TITLE"),
+                action=f"https://www.youtube.com/watch?v={video_ids[randrange(len(video_ids))]}",
+                actiontext=_("HAPPINESS_ACTION_TEXT")
+            )
         else:
-            msg = _("HAPPINESS_SONG", song_link="https://soundcloud.com/mariagrazia84/pharrell-williams-happy")
+            msg = _("HAPPINESS_CHECK_PHONE")
+            response = tell(msg)
+            response.card = Card(
+                title=_("HAPPINESS_TITLE"),
+                subtitle=_("HAPPINESS_SUB_TITLE"),
+                action=f"https://soundcloud.com/mariagrazia84/pharrell-williams-happy",
+                actiontext=_("HAPPINESS_ACTION_TEXT")
+            )
     except requests.exceptions.RequestException as err:
         msg = _('HAPPINESS_REQUEST_ERROR', err=err)
+        response = tell(msg)
 
     # We create a response with either joke or error message
-    return tell(msg)
+    return response
+
+
+
+# Examples to extend the functionality to specific user, couldn't amke it work yet
+
+# from skill_sdk.services.persistence import PersistenceService
+# from skill_sdk import Response, ask, skill, context
+# @skill.intent_handler('MY_BEST_INTENT_EVER')
+# def handle_invoke() -> Response:
+#     session_id = PersistenceService().get()['session_id']
+#     if not session_id:
+#         PersistenceService().set({'session_id': context.session.session_id})
+#         return ask("Hey, first time here? What's your name?")
+#     # Do whatever you want with user, that has already listened to intro
+#     ...
+
+# context_attr = context.attributesV2
+# print(f"Context attributes - {context_attr}")
+# session_id = context.session and context.session.session_id
+# print(session_id)
